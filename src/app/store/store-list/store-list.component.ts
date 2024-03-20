367ea05e9c04046b0e07a356e7229c0f019963aa
Andreas Lauvhjell <andreas.lauvhjell@no.experis.com>
2024-03-20 19:39:01 +0100

    filter applied

--- src/app/store/store-list/store-list.component.ts
+++ src/app/store/store-list/store-list.component.ts
@@ -8,15 +8,29 @@ import { StoreService } from '../store.service';
   styleUrls: ['./store-list.component.css']
 })
 export class StoreListComponent {
-  constructor(private readonly storeService: StoreService) {
+  constructor(private readonly storeService: StoreService) {}
+
+  @Output('newitem') addToCart = new EventEmitter<StoreItem>(); 
+  items: Promise<StoreItem[]> = this.storeService.storeItems;
+  filteredItems: Promise<StoreItem[]> | null = null;
+  filterType: string | null = null;
+
+  async ngOnInit() {
+    this.filteredItems = this.items;
   }
 
+  filterItemsByType(type: string) {
+    if (this.filterType === type) {
+      this.filterType = null; // Reset filter if it's already active
+      this.filteredItems = this.items; // Assign the Promise itself
+    } else {
+      this.filterType = type;
+      this.filteredItems = this.items.then(items => items.filter(item => item.type === type));
+    }
+  }
   
-  @Output('newitem') addToCart = new EventEmitter<StoreItem>(); 
-  items:Promise<StoreItem[]> = this.storeService.storeItems;
 
-
-  updateCart(item:StoreItem){
-    this.addToCart.emit(item)
+  updateCart(item: StoreItem) {
+    this.addToCart.emit(item);
   }
 }
